7d36440f0ff5a51affc1cc2a109eacdad43207b7
StudentKevinOrbie <kevin.orbie@student.kuleuven.be>
2021-03-29 16:25:49 +0200

    Kevin Basic ctrl pipe, EXE/MEM and MEM/WB

--- RTL/cpu.v
+++ RTL/cpu.v
@@ -134,13 +134,45 @@ register_file #(
 ) register_file(
    .clk      (clk               ),
    .arst_n   (arst_n            ),
-   .reg_write(reg_write         ),
+   .reg_write(WB_reg_write      ),
    .raddr_1  (instruction[25:21]),
    .raddr_2  (instruction[20:16]),
    .waddr    (regfile_waddr     ),
    .wdata    (regfile_wdata     ),
    .rdata_1  (regfile_data_1    ),
    .rdata_2  (regfile_data_2    )
+);
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~ Control ~~~~~~~~~~~~~~~~~~~~~~~~~~~
+wire [1:0] WB_ctrl;
+wire [2:0] MEM_ctrl;
+wire [3:0] EXE_ctrl;
+
+always@(*) WB_ctrl = {mem_2_reg, reg_write};
+reg_arstn_en #(.DATA_W(2)) WB_ctrl_pipe_ID_EXE(
+      .clk   (clk       ),
+      .arst_n(arst_n    ),
+      .din   (WB_ctrl   ),
+      .en    (enable    ),
+      .dout  (WB_ctrl_ID_EXE)
+);
+
+always@(*) MEM_ctrl = {alu_src, alu_op, reg_dst};
+reg_arstn_en #(.DATA_W(3)) MEM_ctrl_pipe_ID_EXE(
+      .clk   (clk       ),
+      .arst_n(arst_n    ),
+      .din   (MEM_ctrl   ),
+      .en    (enable    ),
+      .dout  (MEM_ctrl_ID_EXE)
+);
+
+always@(*) EXE_ctrl = {mem_write, mem_read, branch, jump};
+reg_arstn_en #(.DATA_W(4)) EXE_ctrl_pipe_ID_EXE(
+      .clk   (clk       ),
+      .arst_n(arst_n    ),
+      .din   (EXE_ctrl   ),
+      .en    (enable    ),
+      .dout  (EXE_ctrl_ID_EXE)
 );
 
 // ---------------------------------------------- EXE ----------------------------------------------
@@ -183,13 +215,71 @@ branch_unit#(
    .jump_pc      (jump_pc         )
 );
 
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~ Registers ~~~~~~~~~~~~~~~~~~~~~~~~~~~
+reg_arstn_en #(.DATA_W(32)) alu_out_pipe_EXE_MEM(
+      .clk   (clk       ),
+      .arst_n(arst_n    ),
+      .din   (alu_out   ),
+      .en    (enable    ),
+      .dout  (alu_out_EXE_MEM)
+);
+
+reg_arstn_en #(.DATA_W(32)) regfile_data_2_pipe_EXE_MEM(
+      .clk   (clk                  ),
+      .arst_n(arst_n               ),
+      .din   (regfile_data_2_ID_EXE),
+      .en    (enable               ),
+      .dout  (regfile_data_2_EXE_MEM)
+);
+
+reg_arstn_en #(.DATA_W(32)) branch_pc_pipe_EXE_MEM(
+      .clk   (clk       ),
+      .arst_n(arst_n    ),
+      .din   (branch_pc ),
+      .en    (enable    ),
+      .dout  (branch_pc_EXE_MEM)
+);
+
+reg_arstn_en #(.DATA_W(32)) regfile_waddr_pipe_EXE_MEM(
+      .clk   (clk          ),
+      .arst_n(arst_n       ),
+      .din   (regfile_waddr),
+      .en    (enable       ),
+      .dout  (regfile_waddr_EXE_MEM)
+);
+
+reg_arstn_en #(.DATA_W(32)) jump_pc_pipe_EXE_MEM(
+      .clk   (clk       ),
+      .arst_n(arst_n    ),
+      .din   (jump_pc   ),
+      .en    (enable    ),
+      .dout  (jump_pc_EXE_MEM)
+);
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~ Control ~~~~~~~~~~~~~~~~~~~~~~~~~~~
+reg_arstn_en #(.DATA_W(2)) WB_ctrl_pipe_EXE_MEM(
+      .clk   (clk           ),
+      .arst_n(arst_n        ),
+      .din   (WB_ctrl_ID_EXE),
+      .en    (enable        ),
+      .dout  (WB_ctrl_EXE_MEM)
+);
+
+reg_arstn_en #(.DATA_W(1)) zero_flag_pipe_EXE_MEM(
+      .clk   (clk       ),
+      .arst_n(arst_n    ),
+      .din   (zero_flag ),
+      .en    (enable    ),
+      .dout  (zero_flag_EXE_MEM)
+);
+
 // ---------------------------------------------- MEM ----------------------------------------------
 sram #(
    .ADDR_W(10),
    .DATA_W(32)
 ) data_memory(
    .clk      (clk           ),
-   .addr     (alu_out       ),
+   .addr     (alu_out_EXE_MEM),
    .wen      (mem_write     ),
    .ren      (mem_read      ),
    .wdata    (regfile_data_2),
@@ -201,13 +291,50 @@ sram #(
    .rdata_ext(rdata_ext_2   )
 );
 
+reg_arstn_en #(.DATA_W(32)) dram_data_pipe_MEM_WB(
+      .clk   (clk       ),
+      .arst_n(arst_n    ),
+      .din   (dram_data ),
+      .en    (enable    ),
+      .dout  (dram_data_MEM_WB)
+);
+
+reg_arstn_en #(.DATA_W(32)) alu_out_pipe_MEM_WB(
+      .clk   (clk            ),
+      .arst_n(arst_n         ),
+      .din   (alu_out_EXE_MEM),
+      .en    (enable         ),
+      .dout  (alu_out_MEM_WB)
+);
+
+reg_arstn_en #(.DATA_W(32)) regfile_waddr_pipe_MEM_WB(
+      .clk   (clk            ),
+      .arst_n(arst_n         ),
+      .din   (regfile_waddr_EXE_MEM),
+      .en    (enable         ),
+      .dout  (regfile_waddr_MEM_WB)
+);
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~ Control ~~~~~~~~~~~~~~~~~~~~~~~~~~~
+reg_arstn_en #(.DATA_W(2)) WB_ctrl_pipe_MEM_WB(
+      .clk   (clk            ),
+      .arst_n(arst_n         ),
+      .din   (WB_ctrl_EXE_MEM),
+      .en    (enable         ),
+      .dout  (WB_ctrl_MEM_WB)
+);
+
 // ---------------------------------------------- WB ----------------------------------------------
+wire WB_mem_2_reg;
+wire WB_reg_write;
+always@(*) {WB_mem_2_reg, WB_reg_write} = WB_ctrl_MEM_WB;
+
 mux_2 #(
    .DATA_W(32)
 ) regfile_data_mux (
    .input_a  (dram_data    ),
    .input_b  (alu_out      ),
-   .select_a (mem_2_reg     ),
+   .select_a (WB_mem_2_reg     ),
    .mux_out  (regfile_wdata)
 );
 
